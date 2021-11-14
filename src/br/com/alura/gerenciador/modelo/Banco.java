b8b96b099b913c3c8a734773ab8b2996d8b9d880
Patricia <patyamiga16@hotmail.com>
2021-11-14 00:10:48 -0300

    gerenciador

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -1,14 +1,14 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.modelo;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
-	
+
 	private static List<Empresa> lista = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
-	
+
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setId(chaveSequencial++);
@@ -24,19 +24,19 @@ public class Banco {
 		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
-	
-	public List<Empresa> getEmpresas(){
+
+	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
 
 	public void removeEmpresa(Integer id) {
-		
+
 		Iterator<Empresa> it = lista.iterator();
-		
-		while(it.hasNext()) {
+
+		while (it.hasNext()) {
 			Empresa emp = it.next();
-			
-			if(emp.getId() == id) {
+
+			if (emp.getId() == id) {
 				it.remove();
 			}
 		}
@@ -44,7 +44,7 @@ public class Banco {
 
 	public Empresa buscaEmpresaPelaId(Integer id) {
 		for (Empresa empresa : lista) {
-			if(empresa.getId() == id) {
+			if (empresa.getId() == id) {
 				return empresa;
 			}
 		}
