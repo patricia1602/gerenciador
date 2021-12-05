ead88067f668098ae187476329a1896c7bd7667c
Patricia <patyamiga16@hotmail.com>
2021-12-05 03:01:37 -0300

    gerenciador

--- src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -15,6 +16,11 @@ public class ListaEmpresas implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		HttpSession sessao = request.getSession();
+		if(sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+		} 
+		
 		System.out.println("listando empresas");
 
 		Banco banco = new Banco();
